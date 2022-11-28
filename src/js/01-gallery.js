c3105e9844557e3e7cf91276a9ba55208b15d463
Adam Skoczeń <adamsk91@wp.pl>
2022-11-28 19:51:51 +0100

    HW 7 Task 1 tbc

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+for (const img of galleryItems) {
+    const divGallery = document.createElement('div');
+    const linkImg = document.createElement('a');
+    const elementImg = document.createElement('img');
+
+    divGallery.classList.add('gallery__item');
+    linkImg.classList.add('gallery__link');
+    linkImg.href = img.original;
+    elementImg.classList.add('gallery__image');
+    elementImg.dataset.source = img.original;
+    elementImg.src = img.preview;
+    elementImg.alt = img.description;
+
+    linkImg.append(elementImg);
+    divGallery.append(linkImg);
+    gallery.append(divGallery);
+}
