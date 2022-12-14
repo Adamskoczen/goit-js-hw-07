9b5f3da03320a503e3a332e00c6f083a1833eaec
Adam Skoczeń <adamsk91@wp.pl>
2022-12-14 19:20:04 +0100

    123

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,22 +1,44 @@
+import simpleLightbox from '../simplelightbox-master/dist/simple-lightbox.js';
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const gallery = document.querySelector('.gallery');
 
 for (const img of galleryItems) {
-    const divGallery = document.createElement('div');
+    const liImg = document.createElement('li');
     const linkImg = document.createElement('a');
     const elementImg = document.createElement('img');
 
-    divGallery.classList.add('gallery__item');
-    linkImg.classList.add('gallery__link');
+    linkImg.classList.add('gallery__item');
     linkImg.href = img.original;
     elementImg.classList.add('gallery__image');
-    elementImg.dataset.source = img.original;
     elementImg.src = img.preview;
     elementImg.alt = img.description;
 
     linkImg.append(elementImg);
-    divGallery.append(linkImg);
-    gallery.append(divGallery);
+    liImg.append(linkImg);
+    gallery.append(liImg);
+}
+
+gallery.addEventListener("click", onImageClick);
+
+function onImageClick(e) {
+    e.preventDefault();
+    
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
+
+    // const lightbox = new simpleLightbox(`
+    //     <img src="${e.target.dataset.source}" width="800" height="600">
+    // `);
+    const lightbox = new simpleLightbox(
+        `.gallery a`, {sourceAttr}
+        );
+
+    // gallery.addEventListener("keydown", (e) => {
+    //     if (e.code === "Escape") {
+    //         lightbox.close();
+    //     }
+    // });
 }
