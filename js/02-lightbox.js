72be959ebd75806f34580129c5ac0f8e849cd39b
Adam Skoczeń <adamsk91@wp.pl>
2022-12-16 20:06:13 +0100

    Task 2 complete

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,44 +1,16 @@
-import simpleLightbox from '../simplelightbox-master/dist/simple-lightbox.js';
+// import simpleLightbox from '../simplelightbox-master/dist/simple-lightbox.js';
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const gallery = document.querySelector('.gallery');
 
-for (const img of galleryItems) {
-    const liImg = document.createElement('li');
-    const linkImg = document.createElement('a');
-    const elementImg = document.createElement('img');
+galleryItems.forEach((image) => {
+    gallery.insertAdjacentHTML(
+        "beforeend",
+        `<a class="gallery__item" href="${image.original}">
+         <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+        </a>`
+    );
+});
 
-    linkImg.classList.add('gallery__item');
-    linkImg.href = img.original;
-    elementImg.classList.add('gallery__image');
-    elementImg.src = img.preview;
-    elementImg.alt = img.description;
-
-    linkImg.append(elementImg);
-    liImg.append(linkImg);
-    gallery.append(liImg);
-}
-
-gallery.addEventListener("click", onImageClick);
-
-function onImageClick(e) {
-    e.preventDefault();
-    
-    if (e.target.nodeName !== "IMG") {
-        return;
-    }
-
-    // const lightbox = new simpleLightbox(`
-    //     <img src="${e.target.dataset.source}" width="800" height="600">
-    // `);
-    const lightbox = new simpleLightbox(
-        `.gallery a`, {sourceAttr}
-        );
-
-    // gallery.addEventListener("keydown", (e) => {
-    //     if (e.code === "Escape") {
-    //         lightbox.close();
-    //     }
-    // });
-}
+let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
